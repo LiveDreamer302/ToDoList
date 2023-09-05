10ceac9175d1f375ba5937407649d41a0b246af4
Stefan Manoil <manoilstefan302@gmail.com>
2023-09-05 21:42:07 +0300

    End up with identity side

---
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Core.Interfaces;
+using Infrastructure.Services;
+
+namespace API.Extensions;
+
+public static class ApplicationServicesExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddScoped<ITokenService, TokenService>();
+
+        return services;
+    }
+}
